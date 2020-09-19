44dc2cb8882955b9b03aa701e71a6f40889db0dd
mdjuniorislam <70217801+mdjuniorislam@users.noreply.github.com>
2020-09-20 04:18:49 +0600

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+   $('h2').click(function(){
+$('p').slideToggle();
+   });
+});
+
+$(document).ready(function(){
+   $('button').click(function(){
+$('p').slideUp();
+   });
+});
+
+$(document).ready(function(){
+   $('.right button').click(function(){
+$('p').slideDown();
+   });
+});
+
+
+
+
+
+
